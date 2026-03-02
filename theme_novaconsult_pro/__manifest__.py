107c73a6ab31ee6dcbda91d071663ad2b1bbf3ef
Kadama17 <kadama328@gmail.com>
2026-03-02 22:17:57 +0100

    correction ui 1

--- theme_novaconsult_pro/__manifest__.py
+++ theme_novaconsult_pro/__manifest__.py
@@ -61,7 +61,7 @@
     ],
 
     'images': [
-    'static/description/theme_novaconsult_pro_cover.png',
+    'static/description/theme_novaconsult_pro_cover.webp',
     'static/description/screenshot.webp',
 ],
 
